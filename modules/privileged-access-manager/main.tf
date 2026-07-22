7ff12e043214ee53f7e3e95aa3b15311f4d87593
Tyler Young <youngtyler@google.com>
2026-07-21 21:03:08 -0400

    chore: update and restore 2026 Google LLC copyright headers across all codebase files

--- modules/privileged-access-manager/main.tf
+++ modules/privileged-access-manager/main.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2024 Google LLC
+ * Copyright 2026 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
 locals {
   max_request_duration = var.max_request_duration_hours * 60 * 60
   role_bindings        = { for x in var.role_bindings : x.role => x }
